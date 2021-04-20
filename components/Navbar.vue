f210ab9a1ce5a3c144d88aaba0fdf045d365b77b
DevDams <ad.aimedesire@gmail.com>
2021-04-20 09:50:09 +0000

    try fix navbar width

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-screen bg-blue hauteur flex items-center">
-      <div class="w-3/6 m-auto flex items-center justify-between">
-          <div class="logo w-40 h-full flex items-center">
+      <div class="container m-auto flex items-center justify-between">
+          <div class="logo w-36 h-full flex items-center">
               <img src="~/assets/svg/logo-white.svg" alt="">
           </div>
           <div class="navbar_lang">
@@ -20,5 +20,10 @@ export default {
 <style scoped>
 .hauteur {
     height: 4.5rem;
+}
+
+.container {
+    max-width: 1065px;
+    margin: 0 auto;
 }
 </style>
